fdf0a48f23fc075bcb2969b82b1a9d6af86f0513
Cona7 <jedembalce@gmail.com>
2018-11-02 19:15:05 +0100

    init commit
    - snapKit, gitIgnore, swiftLint

--- WorkoutAppSnapKit/ViewController.swift
+++ WorkoutAppSnapKit/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  WorkoutAppSnapKit
-//
-//  Created by Josip Glasovac on 02/11/2018.
-//  Copyright © 2018 Josip Glasovac. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
